f24dc995d1201288c6c0fac5f7ee8b7056ec9b9b
Romtouf <romtouf@laposte.net>
2023-09-22 09:59:04 +0200

    Les zones de textes apparaissent sur le collapse

--- kasa-main/src/components/Collapse.js
+++ kasa-main/src/components/Collapse.js
@@ -3,34 +3,36 @@ import arrowUp from "../assets/arrowUp.svg";
 
 const Collapse = ({ aboutTitle, aboutContent }) => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="collapse">
       <div className="collapseBlock">
         <h3 className="collapseTitle">{aboutTitle}</h3>
         <img
-          onClick={() => setIsOpen(!isOpen)}
+          onClick={handleToggle}
           className={isOpen ? "arrow arrowDown" : "arrow arrowUp"}
           src={arrowUp}
           alt="Flèches pour lire le contenu"
         />
       </div>
-      <p
+      <div className={isOpen ? "collapseText" : "collapseHidden"}>
+        {Array.isArray(aboutContent)
+          ? aboutContent.map((index, text) => {
+              return <span key={index}>{text}</span>;
+            })
+          : aboutContent}
+      </div>
+
+      {/* <p
         id="collapseTextDesign"
         className={isOpen ? "collapseText" : "collapseHidden"}
       >
         {aboutContent}
-      </p>
+      </p> */}
     </div>
   );
 };
 
 export default Collapse;
-
-// {
-//   /* {Array.isArray(content)
-//           ? content.map((id, content) => {
-//               return <span key={id}>{content}</span>;
-//             })
-//           : content} */
-// }
